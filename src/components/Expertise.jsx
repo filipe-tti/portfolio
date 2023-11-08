63884bf0839088c76659005635e801aad585b198
Filipe Silva <52262356+filipe-tti@users.noreply.github.com>
2023-11-08 09:36:31 -0300

    atualizando icones

--- src/components/Expertise.jsx
+++ src/components/Expertise.jsx
@@ -1,22 +1,24 @@
 import "./Expertise.css"
 import {
-  DiJsBadge,
-  DiNodejsSmall,
-  DiReact,
-  DiGithubBadge
+ 
 } from "react-icons/di"
 import {
   SiTypescript,
-  SiSass
+  SiSass,
+  SiFigma,
+  SiReact,
+  SiGithub,
+  SiJavascript,
+ 
 } from "react-icons/si"
 
 const technologies = [
-  {id: "js", name: "JavaScript", icon: <DiJsBadge/>, description: "A linguagem que me cativou por sua versatilidade, dinamismo e sua amplitude para diversas aplicações."},
-  {id: "node", name: "Node.js", icon: <DiNodejsSmall/>, description: " Utilizei o ecossistema de pacotes NPM na maioria dos meus projetos."},
+  {id: "js", name: "JavaScript", icon: <SiJavascript/>, description: "A linguagem que me cativou por sua versatilidade, dinamismo e sua amplitude para diversas aplicações."},
+  {id: "figma", name: "Figma", icon: <SiFigma/>, description: " Utilizei o ecossistema de pacotes NPM na maioria dos meus projetos."},
   {id: "typescript", name: "TypeScript", icon: <SiTypescript/>, description: " Estou me dedicando em aprender mais sobre pois acredito ser uma escolha valiosa para projetos mais robustos e ser uma base confiavel em cima do JavaScript."},
-  {id: "react", name: "React", icon: <DiReact/>, description: " O framework de JS que mais utilizei nos projetos até o momento."},
+  {id: "react", name: "React", icon: <SiReact/>, description: " O framework de JS que mais utilizei nos projetos até o momento."},
   {id: "sass", name: "Sass", icon: <SiSass/>, description: " Considero uma evolução do CSS e gosto muito dos seus recursos como mixins, funções e na sua organização e melhoria da produtividade."},
-  {id: "github", name: "GitHub", icon: <DiGithubBadge/>, description: " Uso o GitHub para hospedar meus projetos pessoais e contribuições em projetos de código aberto."}
+  {id: "github", name: "GitHub", icon: <SiGithub/>, description: " Uso o GitHub para hospedar meus projetos pessoais e contribuições em projetos de código aberto."}
 ]
 
 function TechnologiesContainer() {
