300ab1c1bfc80812c700a6e7b5696e08c7917113
Filipe Silva <52262356+filipe-tti@users.noreply.github.com>
2024-09-05 21:50:07 -0300

    atualizando pra mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,53 +1,78 @@
-import "./Navbar.css"
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import "./Navbar.css";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
+import logo from "../img/logo.png";
 
-import logo from '../img/logo.png'
-
-const logoPng = [logo]
+const logoPng = [logo];
 
 const Navbar = () => {
   return (
     <div className="tela-navbar">
+      <div className="logo">
+        <a href="#">
+          <img src={logoPng} alt="minha logo" />
+        </a>
+      </div>
 
-    <div className="logo">
+      <nav className="menu-desktop">
 
-      <a href="#">
-        <img src={logoPng} alt="minha logo" />
-      </a>
-    </div>
-
-    <nav className="menu-desktop"> 
-
-
-          <nav  className="menu-desktop">
-
-            <lu>
-
-              <AnchorLink className="li" href="#home" offset='100'>Ínicio</AnchorLink>
-              <AnchorLink className="li" href="#expertise" offset='100'>Especialidades</AnchorLink>
-              <AnchorLink className="li" href="#about" offset='100'>Sobre mim</AnchorLink>
-              <AnchorLink className="li" href="#projects" offset='100'>Projetos</AnchorLink>
-
-            </lu>
-
-          </nav>
+        <nav className="menu-desktop">
+          <lu>
+            <AnchorLink className="li" href="#home" offset="100">
+              Ínicio
+            </AnchorLink>
+            <AnchorLink className="li" href="#expertise" offset="100">
+              Especialidades
+            </AnchorLink>
+            <AnchorLink className="li" href="#about" offset="100">
+              Sobre mim
+            </AnchorLink>
+            <AnchorLink className="li" href="#projects" offset="100">
+              Projetos
+            </AnchorLink>
+          </lu>
+        </nav>
 
 
+        
       </nav>
-      <div className="contato-home">
+
+ 
+      <div className="btn-abrir-menu">
+      <i class="bi bi-list"></i>
+     </div>
       
-      <AnchorLink className='btn-contato' href="#contact">Contato</AnchorLink>
+
+      <nav className="menu-mobile">
+
+      <div className="btn-fechar">
+          <i class="bi bi-x-lg"></i>
+      </div>
+      <lu className="lu-mobile"> 
+            <li><a className="li-mobile" href="#home">Ínicio</a></li>
+            <li><a className="li-mobile" href="#expertise">Especialidades</a></li>
+            <li><a className="li-mobile" href="#about">Sobre mim</a></li>
+            <li><a className="li-mobile" href="#projects">Projetos</a></li>
+            <li><a className="li-mobile" href="#home">Contato</a></li>
+          </lu>
+      </nav>
+
+      <div className="overlay-menu">
+
+      </div>
+      
+      
+
+      <div className="contato-home">
+        <nav className="bts">
+          <AnchorLink className="btn-contato" href="#contact">
+            Contato
+          </AnchorLink>
+        </nav>
+      </div>
     </div>
+  );
+};
 
-   
-     
-
-    </div>  
-
-    
-
-  )
-}
-
-export default Navbar
+export default Navbar;
